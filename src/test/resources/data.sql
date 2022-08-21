14f7f6f7066373bb021171810fd5d7fc2075cb39
ishwang <his8457@naver.com>
2022-08-21 15:56:17 +0900

    [data.sql 에러 수정] insert 쿼리 작성 시, 필드명에 (`, backtick) 을 사용하고 value의 (', single quotation)을 사용함.

---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into users(`id`,`name`,`email`,`created_at`,`updated_at`)values(1, 'ishwang', 'ishwang@hictc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into users(`id`,`name`,`email`,`created_at`,`updated_at`)values(2, 'tom', 'tom@hictc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into users(`id`,`name`,`email`,`created_at`,`updated_at`)values(3, 'maria', 'maria@hictc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into users(`id`,`name`,`email`,`created_at`,`updated_at`)values(4, 'robert', 'robert@hictc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into users(`id`,`name`,`email`,`created_at`,`updated_at`)values(5, 'jeshi', 'jeshi@hnaver.com', now(), now());
