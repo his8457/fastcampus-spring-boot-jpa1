09592a2a9422c210b474b8ae4832c4c6ad471a18
ishwang <his8457@naver.com>
2022-08-21 15:57:51 +0900

    [User 객체 조작] User Entity를 insert, select 해봄. .save 로 insert, .find 로 조회

--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -1,9 +1,13 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,8 +18,15 @@ class UserRepositoryTest {
 
     @Test
     void crud(){//create, read, update, delete
-        userRepository.save(new User());
+//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+//        List<User> users = userRepository.findAllById(Lists.newArrayList(1l, 3l, 5l));
+        User user1 = new User("Tim", "Tim@email.com");
+        User user2 = new User("Kim","Kim@email.com");
 
-        System.out.println(">>> " + userRepository.findAll());
+        userRepository.saveAll(Lists.newArrayList(user1, user2));
+
+        List<User> result = userRepository.findAll();
+
+        result.forEach(System.out::println);
     }
 }
